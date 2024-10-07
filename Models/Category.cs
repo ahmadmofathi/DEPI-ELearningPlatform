deb76b69b76f0703f5c8897f5f79709f57a7b425
Mohamed_Swelam19 <130258605+Mohamedkhaled119@users.noreply.github.com>
2024-10-07 21:10:24 +0300

    Uploaded Courses Models

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Depi_Final_Project.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string CategoryName { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
+
---
+++ Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Depi_Final_Project.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        [MaxLength(100)]
+        [Required]
+        public string CourseName { get; set; }
+
+        public string Description { get; set; }
+
+        public int NoOfVideos { get; set;}
+        public int Price { get; set; }
+        public int CategoryId {  get; set; }
+        public Category Category { get; set; }
+        public List<Video> Videos { get; set; }
+    }
+}

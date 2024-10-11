6e58376df1a3813c534c9bbb43b65abc581caf12
Mohamed Elkhodary <Mohamed Elkhodary@DESKTOP-RJP9CGE>
2024-10-12 02:00:36 +0300

    N tier architecture

---
+++ E-LearningPlatform.DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using E_LearningPlatform.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_LearningPlatform.DataAccess.Context
+{
+    public class AppDbContext :DbContext 
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Request> requests { get; set; }
+
+        public DbSet<Enrollment> Enrollments  { get; set; }
+    }
+}
